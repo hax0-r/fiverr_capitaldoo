5488036b79c487f91f9e32179962a2af57068134
hax0-r <talha18513@gmail.com>
2025-02-22 10:32:03 +0500

    changes done

--- Docs/Javascripts/footer.js
+++ Docs/Javascripts/footer.js
@@ -7,8 +7,9 @@ footer.innerHTML = `
             <div
                 class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-3 md:gap-8 py-10 max-sm:max-w-sm max-sm:mx-auto gap-y-8">
                 <div class="col-span-full mb-10 lg:col-span-2 lg:mb-0">
-                    <a href="https://pagedone.io/" class="flex justify-center lg:justify-start">
-                        logo
+                    <a href="./index.html" class="flex justify-center lg:justify-start">
+                        <img src="https://capitaldoo.com/trading-platform/metatrader5/images/Man_Utd_x_Doo_Prime_desktop.png"
+                class="md:h-12 h-8" alt="">
                     </a>
                     <p class="pt-8 block pb-3 text-sm lg:max-w-xs text-center lg:text-left font-semibold text-black">
                         Contac Us </p>
